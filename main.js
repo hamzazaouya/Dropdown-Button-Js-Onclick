2b04043f82a4352312c07f962fb7efe727231818
hamza <hamzazaouya18@gmail.com>
2020-09-09 14:53:52 +0100

    final editing

---
+++ main.js
@@ -0,0 +1,11 @@
+/* global document */
+
+
+function dropShow() {
+    
+    'use strict';
+    
+    document.getElementById('dropdown').classList.toggle('show');
+    
+    document.getElementById('down').classList.toggle('down');
+}
